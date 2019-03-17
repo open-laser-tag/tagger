431cc1b46039395f1a419378a2fad244d0a6fbb1
Florian <florian.hahn@posteo.de>
2019-03-17 12:03:22 +0100

    only send error message when trigger is pressed

--- tagger_firmware/ir_and_trigger.cpp
+++ tagger_firmware/ir_and_trigger.cpp
@@ -94,7 +94,7 @@ void refresh_trigger_status(void * parameter) {
     usblog.debugln("sending trigger status via bt");
     trigger_char->setValue((int&)trigger.pressed);
     ble_notify(trigger_char);
-    if (!device_connected) {
+    if (!device_connected && trigger.pressed) {
       uint8_t err_msg[5] = ERROR_MSG;
       usblog.infoln("Device not conntected. Sending error message 0xFFFFFF via IR");
       ir.write((const unsigned char*)err_msg, sizeof(err_msg));
