98f9a41f263a8e06610c589ad86f31dcfe42953c
JulesDoe <JulesDoe@users.noreply.github.com>
2019-12-17 20:55:41 +0100

    cathegorized defines

--- src/tagger.h
+++ src/tagger.h
@@ -8,29 +8,22 @@
 #ifndef tagger_h
 #define tagger_h
 
+/* ++++++ */
 /* config */
 //this is a hardware identifier for FastLED
 #define ESP32
+
+/* RGB LEDs */
 //choose one LED type
 //#define LED_TYPE_APA102  
-#define LED_TYPE_NEOPIXEL  
+#define LED_TYPE_NEOPIXEL 
+
 #define LED_CLOCK_PIN 18 //only used for spi leds like APA102
 #define LED_DATA_PIN 19
 #define NUM_LEDS 4
 #define LED_OVERALL_BRIGHTNESS 20 //scale 0 to 255
 //FASTLED_ESP32_I2S is defined to tell FastLED to use I2S driver instead of RMT. Reference for this option in FastLED/platforms/esp/32/clockless_i2s_esp32.h. There is a conflict when using RMT with both LED and IR, although it should be possible according to the options in FastLED/platforms/esp/32/clockless_rmt_esp32.h.
 #define FASTLED_ESP32_I2S true
-//I don't know why this is needed. It is for IRremote.h/IRremoteInt.h
-#define ARDUINO 101
-#define IR_RECV_FRONT_PIN 27
-#define IR_RECV_RIGHT_PIN 26
-#define IR_RECV_LEFT_PIN 25
-#define PIN_TRIGGER 21
-#define ONBOARDLED_PIN 2
-#define PLAYER_DOWNTIME_IN_MS 3000
-#define TEAM_SELECTION_TIME_IN_MS 5000
-#define IR_RMT_TX_CHANNEL RMT_CHANNEL_6 /*!< RMT channel for transmitter */
-#define IR_RMT_TX_GPIO_NUM GPIO_NUM_22  /*!< GPIO number for transmitter signal */
 #define LED_INDEX_BT 0
 #define LED_INDEX_PLAYER_STATUS 1
 #define LED_INDEX_TEAM 2
@@ -39,8 +32,28 @@
 #define COLOR_BT_CONNECTION_OFF 0xFF0000
 #define COLOR_PLAYER_STATUS_ON 0x00FF00
 #define COLOR_PLAYER_STATUS_OFF 0xFF0000
+#define ONBOARDLED_PIN 2
+
+/* Infra Red */
+//I don't know why this is needed. It is for IRremote.h/IRremoteInt.h
+#define ARDUINO 101
+#define IR_RECV_FRONT_PIN 27
+#define IR_RECV_RIGHT_PIN 26
+#define IR_RECV_LEFT_PIN 25
+#define IR_RMT_TX_CHANNEL RMT_CHANNEL_6 /*!< RMT channel for transmitter */
+#define IR_RMT_TX_GPIO_NUM GPIO_NUM_22  /*!< GPIO number for transmitter signal */
+
+/* Trigger Button */
+#define PIN_TRIGGER 21
 #define DEBOUNCETIME 10
 
+
+/* Timings */
+#define PLAYER_DOWNTIME_IN_MS 3000
+#define TEAM_SELECTION_TIME_IN_MS 5000
+
+
+/* ++++++++ */
 /* includes */
 #include <Arduino.h>
 #include <BLEDevice.h>
@@ -54,6 +67,8 @@
 #include "Esp32_infrared_nec.h"
 #include <esp_log.h>
 
+
+/* ++++++ */
 /* macros */
 #define HARDWARE_SERIAL0 0 // rx_pin=3, tx_pin=1 (usb)
 #define HARDWARE_SERIAL1 1 // don't use this one
